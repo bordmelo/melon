4176f88fa983a7961e25bff10ec22912847c1611
Lucas Melo <lucasmelo@protonmail.com>
2020-01-09 16:57:39 -0300

    Add view system

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -3,7 +3,15 @@
 namespace App\Controllers;
 
 class HomeController {
+
+  private $view;
+
+  public function __construct() {
+    $this->view = new \stdClass;
+  }
+
   public function index() {
-    echo "Home Index";
+    $this->view->name = "Lucas Melo";
+    require_once __DIR__ . "/../views/home/index.phtml";
   }
 }
